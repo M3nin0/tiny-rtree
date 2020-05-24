ac1a46de533dd91af14f4b3d3b3f91f9161bf240
nataliapedroso <31451924+nataliapedroso@users.noreply.github.com>
2020-05-24 15:09:32 -0300

    Update main.cpp

--- rtree/main.cpp
+++ rtree/main.cpp
@@ -10,16 +10,16 @@ int main()
     // Testando os primeiros passos da inserção
     RTree rTree(2, 4);
     rTree.insert(new BaseRectangle(0, 3, 0, 5));
-    rTree.print();
+    rTree.print(); // imprime normal
     rTree.insert(new BaseRectangle(3, 6, 0, 1));
     rTree.insert(new BaseRectangle(7, 16, 5, 51));
-    rTree.print();
+    rTree.print(); // imprime normal
     rTree.insert(new BaseRectangle(14, 16, 51, 510));
-    rTree.print();
+    rTree.print(); // também imprime normal
     
     // Adiciona geometria com o nó raiz cheio
     rTree.insert(new BaseRectangle(12, 13, 15, 21));
-    rTree.print(); // depois daqui não imprime mais, ele entra na função print e na função print_ mas nao mostra nada
+    rTree.print(); // depois daqui não imprime mais, ele entra na função print e na função print_ mas nao mostra nada do mbr
 
     // Gerando segunda camada cheia
     rTree.insert(new BaseRectangle(600, 620, 34, 67));
@@ -28,7 +28,9 @@ int main()
     rTree.print(); // idem
 
     // Testando a busca 
-    rTree.search(new BaseRectangle(12,14,16,22)); // era pra imprimir pelo menos um endereço
+    rTree.search(new BaseRectangle(12,14,16,22)); // era pra imprimir pelo menos um endereço,
+                                                // mas seguindo o erro que deu na print_, quando chega na folha, ele nao
+                                                // acha nada do mbr delas, aparentemente
 
     return 0;
 }
