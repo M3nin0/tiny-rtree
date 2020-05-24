8f867cd9dfc84fca7b6f9c3ea4358ea8503fb5bd
nataliapedroso <31451924+nataliapedroso@users.noreply.github.com>
2020-05-24 14:58:49 -0300

    Update main.cpp

--- rtree/main.cpp
+++ rtree/main.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include "rtree.hpp"
 #include "geometry.hpp"
-#include "rtree.cpp"
-#include "geometry.cpp"
+// sei que é errado, só uso isso pra rodar tudo de uma vez pela main hahaha
+// #include "rtree.cpp"
+// #include "geometry.cpp"
 
 int main()
 {
     // Testando os primeiros passos da inserção
     RTree rTree(2, 4);
     rTree.insert(new BaseRectangle(0, 3, 0, 5));
+    rTree.print();
     rTree.insert(new BaseRectangle(3, 6, 0, 1));
     rTree.insert(new BaseRectangle(7, 16, 5, 51));
+    rTree.print();
     rTree.insert(new BaseRectangle(14, 16, 51, 510));
+    rTree.print();
     
     // Adiciona geometria com o nó raiz cheio
     rTree.insert(new BaseRectangle(12, 13, 15, 21));
+    rTree.print(); // depois daqui não imprime mais, ele entra na função print e na função print_ mas nao mostra nada
 
     // Gerando segunda camada cheia
     rTree.insert(new BaseRectangle(600, 620, 34, 67));
+    rTree.print(); // o mesmo, só entra na print e print_ mas nao imprime nada
     rTree.insert(new BaseRectangle(89, 98, 78, 99));
+    rTree.print(); // idem
 
-    // Testando a busca
-    rTree.search(new BaseRectangle(12,14,16,22));
+    // Testando a busca 
+    rTree.search(new BaseRectangle(12,14,16,22)); // era pra imprimir pelo menos um endereço
 
     return 0;
 }
