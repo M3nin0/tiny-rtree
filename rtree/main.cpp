97c9def97ddcf214655c2c07d743fcf48e4daaeb
nataliapedroso <31451924+nataliapedroso@users.noreply.github.com>
2020-06-06 16:27:41 -0300

    Update main.cpp

--- rtree/main.cpp
+++ rtree/main.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 #include "rtree.hpp"
 #include "geometry.hpp"
-// sei que é errado, só uso isso pra rodar tudo de uma vez pela main hahaha
-// #include "rtree.cpp"
-// #include "geometry.cpp"
 
 int main()
 {
     // Testando os primeiros passos da inserção
     RTree rTree(2, 4);
     rTree.insert(new BaseRectangle(0, 3, 0, 5));
-    rTree.print(); // imprime normal
+    std::cout << "print 1" << std::endl; //teste
+    rTree.print();
     rTree.insert(new BaseRectangle(3, 6, 0, 1));
     rTree.insert(new BaseRectangle(7, 16, 5, 51));
-    rTree.print(); // imprime normal
+    std::cout << "print 2" << std::endl;
+    rTree.print();
     rTree.insert(new BaseRectangle(14, 16, 51, 510));
-    rTree.print(); // também imprime normal
+    std::cout << "print 3" << std::endl;
+    rTree.print();
     
     // Adiciona geometria com o nó raiz cheio
     rTree.insert(new BaseRectangle(12, 13, 15, 21));
-    rTree.print(); // depois daqui não imprime mais, ele entra na função print e na função print_ mas nao mostra nada do mbr
+    std::cout << "print 4" << std::endl;
+    rTree.print();
 
     // Gerando segunda camada cheia
     rTree.insert(new BaseRectangle(600, 620, 34, 67));
-    rTree.print(); // o mesmo, só entra na print e print_ mas nao imprime nada
+    std::cout << "print 5" << std::endl;
+    rTree.print();
     rTree.insert(new BaseRectangle(89, 98, 78, 99));
-    rTree.print(); // idem
+    std::cout << "print 6" << std::endl;
+    rTree.print();
 
     // Testando a busca 
-    rTree.search(new BaseRectangle(12,14,16,22)); // era pra imprimir pelo menos um endereço,
-                                                // mas seguindo o erro que deu na print_, quando chega na folha, ele nao
-                                                // acha nada do mbr delas, aparentemente
-
+    rTree.search(new BaseRectangle(12,14,16,22));
+    
     return 0;
 }
