c17cd409475e8a29cd33ee256f46c3af41f1fd45
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-06-12 00:01:31 -0300

    Reorganização da estrutura da suíte de testes

---
+++ benchmark/tests/search.hpp
@@ -0,0 +1,11 @@
+#include "../../rtree/tree.hpp"
+#include "../../rtree/split.hpp"
+#include "../../rtree/geometry.hpp"
+
+#include <chrono>
+#include <iostream>
+
+void doSearchTest()
+{
+    std::cout << "Teste de busca" << std::endl;
+}
